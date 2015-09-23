b6a321153c74268f45dd243813342fbc4868ff4c
Henrique Chaltein <hxs1151@rit.edu>
2015-09-22 21:09:59 -0400

    Project Settings for one player.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Reads every player input from the controller.
+public class InputManager : MonoBehaviour {
+
+    // Player Keys
+	
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
