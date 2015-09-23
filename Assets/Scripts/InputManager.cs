6d88dcd3c948e14b159c75532f6c27981ef25f18
Henrique Chaltein <hxs1151@rit.edu>
2015-09-22 22:04:24 -0400

    Basic Input System

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,16 +5,130 @@ using System.Collections;
 // Reads every player input from the controller.
 public class InputManager : MonoBehaviour {
 
-    // Player Keys
-	
+    // Class Variables
+    [SerializeField]
+    [Range(1,2)]
+    public int playerID = 1;
+
+    // Component Variables
+    RobotSyncBehavior RobotSyncScrpt;
+
+    //Test Variables
+    public string AxisName;
+    public bool AxisState;
+
+    void Start ()
+    {
+        RobotSyncScrpt = GetComponent<RobotSyncBehavior>();
+    }
     
-    // Use this for initialization
-	void Start () {
-	
+   	// Update is called once per frame
+	void Update ()
+    {
+        ReadPlayerInput();  // Reads the player input
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Read a single Player ID
+    void ReadPlayerInput()
+    {
+        // Key variables.
+        string keyRead;     // String cointaining the Read Key.
+        bool keyState = false;      // Logic state of that key.
+
+        // Forward_P axis
+        keyRead = "Forward_P" + playerID.ToString();
+        if (Input.GetAxis(keyRead) >0)
+        {
+            keyState = true;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+        else if (Input.GetAxis(keyRead) < 0)
+        {
+            keyState = false;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+
+        // Sideways_P axis
+        keyRead = "Sideways_P" + playerID.ToString();
+        if (Input.GetAxis(keyRead) > 0)
+        {
+            keyState = true;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+        else if (Input.GetAxis(keyRead) < 0)
+        {
+            keyState = false;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+
+        // LeftPunch_P axis
+        keyRead = "LeftPunch_P" + playerID.ToString();
+        if (Input.GetButtonUp(keyRead))
+        {
+            keyState = true;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+        else if (Input.GetButtonDown(keyRead))
+        {
+            keyState = false;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+
+        // RightPunch_P axis
+        keyRead = "RightPunch_P" + playerID.ToString();
+        if (Input.GetButtonUp(keyRead))
+        {
+            keyState = true;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+        else if (Input.GetButtonDown(keyRead))
+        {
+            keyState = false;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+
+        // LeftKick_P axis
+        keyRead = "LeftKick_P" + playerID.ToString();
+        if (Input.GetButtonUp(keyRead))
+        {
+            keyState = true;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+        else if (Input.GetButtonDown(keyRead))
+        {
+            keyState = false;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+
+        // RightKick_P axis
+        keyRead = "RightKick_P" + playerID.ToString();
+        if (Input.GetButtonUp(keyRead))
+        {
+            keyState = true;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+        else if (Input.GetButtonDown(keyRead))
+        {
+            keyState = false;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+
+        // Block_P axis
+        keyRead = "Block_P" + playerID.ToString();
+        if (Input.GetButtonUp(keyRead))
+        {
+            keyState = true;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+        else if (Input.GetButtonDown(keyRead))
+        {
+            keyState = false;
+            RobotSyncScrpt.ReceiveInput(keyRead, playerID, keyState);
+        }
+
+        AxisName = keyRead;
+        AxisState = keyState;
+
+    }
 }
