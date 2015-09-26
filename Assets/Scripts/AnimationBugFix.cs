4a5dff8f24eb353415cc632b83ece9f4e64cb337
Henrique Chaltein <hxs1151@rit.edu>
2015-09-26 12:52:33 -0400

    "Fixed" Animation movement bug.

---
+++ Assets/Scripts/AnimationBugFix.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This Script is jut o stop the movement of the body bug.
+public class AnimationBugFix : MonoBehaviour
+{
+    Transform RobotMesh;
+
+	// Use this for initialization
+	void Start ()
+    {
+        RobotMesh = transform.FindChild("RobotMesh");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 offsetVec = new Vector3(0.5f, 0.5f, 0.5f);
+        if(RobotMesh.localPosition.x > offsetVec.x ||
+            RobotMesh.localPosition.y > offsetVec.y ||
+            RobotMesh.localPosition.z > offsetVec.z)
+        {
+            RobotMesh.localPosition = Vector3.zero;
+        }
+	}
+}
